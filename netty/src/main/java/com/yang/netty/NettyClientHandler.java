050551cdbd8356a0d0efb3852a11e40fb1c39a59
qiuyangshen <Sqy545498216>
2021-05-21 19:41:58 +0800

    手写rpc框架

--- netty/src/main/java/com/yang/netty/NettyClientHandler.java
+++ netty/src/main/java/com/yang/netty/NettyClientHandler.java
@@ -1,23 +1,25 @@
 package com.yang.netty;
 
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.concurrent.EventExecutorGroup;
 
-import java.util.UUID;
+import java.util.Date;
 
-public class NettyServerHandler extends SimpleChannelInboundHandler<String> {
-
-
+public class NettyClientHandler extends SimpleChannelInboundHandler<String> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
-        System.out.println(ctx.channel().remoteAddress()+","+ msg);
-        ctx.channel().writeAndFlush("from service"+ UUID.randomUUID());
+        System.out.println(ctx.channel().remoteAddress());
+        System.out.println("client output"+ msg);
+        ctx.writeAndFlush("from client:"+new Date());
+
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("来自服务端的问候");
+        System.out.println("来自客户端的问候");
+        ctx.writeAndFlush("from client:"+new Date());
     }
 
     @Override
--- netty/src/main/java/com/yang/netty/NettyServer.java
+++ netty/src/main/java/com/yang/netty/NettyServer.java
@@ -1,34 +1,43 @@
 package com.yang.netty;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.CharsetUtil;
 
 public class NettyServer {
 
     public static void main(String[] args) {
-        //bossGroup主要用于处理连接请求
-//        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
-//        EventLoopGroup workGroup = new NioEventLoopGroup(8);
-//
-//        try{
-//            ServerBootstrap bootstrap = new ServerBootstrap();
-//            bootstrap.group(bossGroup, workGroup)
-//                    .channel(NioServerSocketChannel.class)
-//                    .option(ChannelOption.SO_BACKLOG, 1024)
-//                    .childHandler(new ChannelInitializer<>() {
-//
-//                        @Override
-//                        protected void initChannel(Channel ch) throws Exception {
-//                            ch.pipeline().addLast(new NettyServerHandler());
-//                        }
-//                    })
-//
-//        }
+//        bossGroup主要用于处理连接请求
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workGroup = new NioEventLoopGroup(8);
+
+        try{
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
+                            pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
+                            pipeline.addLast(new NettyServerHandler());
+                        }
+                    });
+            ChannelFuture channelFuture = bootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
     }
 
 
